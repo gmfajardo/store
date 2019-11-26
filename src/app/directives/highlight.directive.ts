ded11de281ed11ec1f368a856f46e12b0e21118a
gmfajardo <gmcrls13@gmail.com>
2019-11-26 17:10:24 -0400

    Using custom directives

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  @Input() bgcolor: string = 'red';
+  constructor(private elementRef: ElementRef) {
+    elementRef.nativeElement.style.backgroundColor = this.bgcolor;
+    console.log('Color' + this.bgcolor);
+   }
+
+}
