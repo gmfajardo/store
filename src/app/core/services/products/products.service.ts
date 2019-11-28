3d8155cfbc3cf24485bf19754e9decc989fa4fa4
Giame Fajardo <giame_fajardo@cecomsa.com>
2019-11-28 18:08:22 -0400

    Using enviroment variables

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from 'src/app/Product';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -55,10 +56,10 @@ export class ProductsService {
   constructor(private httpClient: HttpClient) { }
   getAll() {
     // return this.products;
-    return this.httpClient.get<Product[]>('http://platzi-store.herokuapp.com/products');
+    return this.httpClient.get<Product[]>(`${environment.api_url}/products`);
   }
   getProduct(id: string) {
-    return this.httpClient.get<Product>(`http://platzi-store.herokuapp.com/products/${id}`);
+    return this.httpClient.get<Product>(`${environment.api_url}/products/${id}`);
     // return this.products.find(item =>
     //   item.id === id
     // );
