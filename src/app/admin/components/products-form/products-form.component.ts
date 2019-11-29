ec2bd85d88700cda2617594374379c8c47e83d74
gmfajardo <gmcrls13@gmail.com>
2019-11-29 14:35:01 -0400

    Creacion de producto por formulario

--- src/app/admin/components/products-form/products-form.component.ts
+++ src/app/admin/components/products-form/products-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductsService } from 'src/app/core/services/products/products.service';
+import { Router } from '@angular/router';
+import { ProductsListComponent } from '../products-list/products-list.component';
 
 @Component({
   selector: 'app-products-form',
@@ -10,8 +13,9 @@ export class ProductsFormComponent implements OnInit {
 
   form: FormGroup;
   constructor(
-    private formBuilder: FormBuilder
-  ) {
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router) {
     this.buildForm();
   }
 
@@ -26,5 +30,17 @@ export class ProductsFormComponent implements OnInit {
       image: [''],
       description: ['', [Validators.required]],
     });
+  }
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const newProduct = this.form.value;
+      this.productsService.createProduct(newProduct)
+      .subscribe((resultProduct) => {
+        console.log('Result: ' + resultProduct);
+        this.router.navigate(['/admin/products']);
+      });
+    }
+    console.log(this.form.value);
   }
 }
