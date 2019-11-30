eacb9e149c97bb81296a205ae5926097487e7fbb
gmfajardo <gmcrls13@gmail.com>
2019-11-30 10:37:48 -0400

    Agregando al carrito con un pipe asincronamente

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart/cart.service';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,17 +10,14 @@ import { map } from 'rxjs/operators';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
   constructor(
     private cartService: CartService
   ) {
-    this.cartService.cart$
+    this.total$ = this.cartService.cart$
     .pipe(
       map(products => products.length)
-    )
-    .subscribe((total) => {
-      this.total = total;
-    });
+    );
   }
 
   ngOnInit() {
