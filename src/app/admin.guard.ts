9a0f67595f4b913806056e403c1c51a85295ae7b
Giame Fajardo <gmcrls13@gmail.com>
2019-12-04 00:10:15 -0400

    Guard redirecting

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './core/services/auth/auth.service';
 import { map, tap } from 'rxjs/operators';
@@ -9,14 +9,19 @@ import { map, tap } from 'rxjs/operators';
 })
 export class AdminGuard implements CanActivate {
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      tap(user => console.log(user)),
-      map(user => user ===null ? false : true)
+      map(user => user ===null ? false : true),
+      tap(hasUser => {
+        if(!hasUser) {
+          this.router.navigate(['./auth/login'])
+        } 
+      }),
     );
   }
 }
